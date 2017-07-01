0b07621def347af7f01fccd07e45e3f77f4b583a
Will保哥 <doggy.huang@gmail.com>
2017-07-01 11:15:51 +0800

    06 練習安裝 Swashbuckle 自動產生互動式 API 文件

--- DCT_106025/App_Start/SwaggerConfig.cs
+++ DCT_106025/App_Start/SwaggerConfig.cs
@@ -2,6 +2,7 @@ using System.Web.Http;
 using WebActivatorEx;
 using DCT_106025;
 using Swashbuckle.Application;
+using System;
 
 [assembly: PreApplicationStartMethod(typeof(SwaggerConfig), "Register")]
 
@@ -57,7 +58,7 @@ namespace DCT_106025
                         //c.BasicAuth("basic")
                         //    .Description("Basic HTTP Authentication");
                         //
-						// NOTE: You must also configure 'EnableApiKeySupport' below in the SwaggerUI section
+                        // NOTE: You must also configure 'EnableApiKeySupport' below in the SwaggerUI section
                         //c.ApiKey("apiKey")
                         //    .Description("API Key Authentication")
                         //    .Name("apiKey")
@@ -97,7 +98,7 @@ namespace DCT_106025
                         // those comments into the generated docs and UI. You can enable this by providing the path to one or
                         // more Xml comment files.
                         //
-                        //c.IncludeXmlComments(GetXmlCommentsPath());
+                        c.IncludeXmlComments(GetXmlCommentsPath());
 
                         // Swashbuckle makes a best attempt at generating Swagger compliant JSON schemas for the various types
                         // exposed in your API. However, there may be occasions when more control of the output is needed.
@@ -241,6 +242,12 @@ namespace DCT_106025
                         //
                         //c.EnableApiKeySupport("apiKey", "header");
                     });
+        }
+
+        private static string GetXmlCommentsPath()
+        {
+            return String.Format("{0}/App_Data/DCT_106025.xml",
+                AppDomain.CurrentDomain.BaseDirectory);
         }
     }
 }
